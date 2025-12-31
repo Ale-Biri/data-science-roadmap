1294d83844e483f8a1d719505d8bda24cfc41021
Ale-Biri <alesantosmartin00@gmail.com>
2025-12-31 17:18:24 +0100

    Add Python script to demonstrate string manipulation techniques

---
+++ Fase 0 Python/0_work_with_strings.py
@@ -0,0 +1,9 @@
+
+phrase = "Giraffe Academy"
+print(phrase + " is cool") #Añado la variable phrase a otra cadena
+print(phrase.lower()) #Convierto a minúsculas
+print(phrase.upper()) #Convierto a mayúsculas
+print(len(phrase)) #Saco los caracteres de la variable phrase
+print(phrase[0]) #Accedo a un carácter concreto de la variable phrase
+print(phrase.index("G")) #Busco el índice de un carácter concreto
+print(phrase.replace("Giraffe", "Elephant")) #Reemplazo una parte de la cadena por otra
