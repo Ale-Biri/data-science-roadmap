3b39bcabf50f868b195d23a81776d8f0c328dac0
Ale-Biri <alesantosmartin00@gmail.com>
2026-04-10 19:50:43 +0200

    Add name and age input and update greeting message in input script

--- Fase 0 Python/0_inputs.py
+++ Fase 0 Python/0_inputs.py
@@ -1,2 +1,3 @@
 name = input("Enter your name: ") #Almaceno el valor que el usuario ingresa en la variable "name"
-print("Hello " + name + "!") #Imprimo un mensaje de bienvenida con el texto indicado por el usuario
+age = input("Enter your age: ") #Almaceno el valor que el usuario ingresa en la variable "age"
+print("Hello " + name + "! You are " + age + ".") #Imprimo un mensaje de bienvenida con el texto indicado por el usuario
