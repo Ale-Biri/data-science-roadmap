9c7de2022e20e51e1f3102e934f517dc2b62c529
Ale-Biri <alesantosmartin00@gmail.com>
2026-04-10 20:40:08 +0200

    Add list manipulation script to demonstrate basic list operations in Python

---
+++ Fase 0 Python/0_list.py
@@ -0,0 +1,10 @@
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"] # Crea una lista de amigos
+friends[1] = "Mike" # Cambia el segundo elemento de la lista a "Mike"
+print(friends) # Imprime la lista completa
+print(friends[0]) # Imprime el primer elemento de la lista
+print(friends[2]) # Imprime el tercer elemento de la lista
+print(friends[-1]) # Imprime el último elemento de la lista
+print(friends[-2]) # Imprime el penúltimo elemento de la lista
+print(friends[1:]) # Imprime desde el segundo elemento hasta el final
+print(friends[1:4]) # Imprime desde el segundo elemento hasta el cuarto (excluyendo el cuarto)
+print(friends[1]) # Imprime el segundo elemento de la lista, que es "Mike" después de la modificación.
