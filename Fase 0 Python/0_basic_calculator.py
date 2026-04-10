b177bcd7c98a58ef8d8f9915eae5d43eeaa546e5
Ale-Biri <alesantosmartin00@gmail.com>
2026-04-10 20:12:31 +0200

    Add basic calculator script to sum two floating-point numbers

---
+++ Fase 0 Python/0_basic_calculator.py
@@ -0,0 +1,7 @@
+num1 = input("Enter a number: ") #Almaceno el valor que el usuario ingresa en la variable "num1"
+num2 = input("Enter another number: ") #Almaceno el valor que el usuario ingresa en la variable "num2"
+# result = num1 + num2 | Suma los valores de "num1" y "num2" y almacena el resultado en la variable "result" y muestra una cadena
+# result = int(num1) + int(num2) | Convierte los valores de "num1" y "num2" a enteros, los suma y almacena el resultado en la variable "result". NO PERMITE DECIMALES
+result = float(num1) + float(num2) # Convierte los valores de "num1" y "num2" a flotantes, los suma y almacena el resultado en la variable "result". PERMITE DECIMALES
+
+print(result) #Imprime el resultado de la suma
