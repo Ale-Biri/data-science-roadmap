196837a053db1c8fbcf617e2675540c15366bf1c
Ale-Biri <alesantosmartin00@gmail.com>
2026-04-10 19:41:58 +0200

    Add initial Python scripts for working with numbers and strings

---
+++ Fase 0 Python/0_work_with_numbers.py
@@ -0,0 +1,13 @@
+from math import * # importo todas las funciones matemáticas disponibles en la biblioteca math
+
+my_num = -5
+
+print(str(my_num) + " is my favourite number") #Convierto el número en un string para poder concatenarlo con otra cadena
+print(abs(my_num)) #Saco el valor absoluto del número
+print(pow(3, 2)) #Elevo el número 3 a la potencia de 2
+print(max(4, 6)) #Saco el número más grande entre 4 y 6
+print(min(4, 6)) #Saco el número más pequeño entre 4 y 6
+print(round(3.7)) #Redondeo el número 3.7 al número entero más cercano
+print(floor(3.7)) #Redondeo el número 3.7 hacia abajo al número entero más cercano
+print(ceil(3.7)) #Redondeo el número 3.7 hacia arriba al número entero más cercano
+print(sqrt(36)) #Saco la raíz cuadrada de 36
