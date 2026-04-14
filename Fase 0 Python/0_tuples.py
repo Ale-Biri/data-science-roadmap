9c9ac835c2ad8d92861255710807cd059f2c48c9
Ale-Biri <alesantosmartin00@gmail.com>
2026-04-14 17:57:11 +0200

    Add tuple example demonstrating immutability and element access in Python

---
+++ Fase 0 Python/0_tuples.py
@@ -0,0 +1,4 @@
+coordinates = (4, 5) #Creo una tupla con dos números, las cuales son inmutables, es decir, no se pueden modificar después de su creación
+
+print(coordinates[0]) #Imprimo el primer número de la tupla
+print(coordinates[1]) #Imprimo el segundo número de la tupla
